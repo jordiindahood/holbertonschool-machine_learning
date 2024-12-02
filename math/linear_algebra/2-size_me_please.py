6ad16c5dfbd48ea2b84ae2461905234602c32d46
jordiindahood <hazemouni123@gmail.com>
2024-12-02 18:28:15 +0100

    task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
+"""
+module file for matrix_shape
+
+"""
 
 
 def matrix_shape(matrix):
+    """a function taht returns the shape of a matrix
+
+    Keyword arguments:
+    matrix -- ndarray
+    Return: list of integers
+    """
+
     shape = []
     while matrix:
         try:
