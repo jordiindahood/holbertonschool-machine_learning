e76c5769cacb4d3046511489edff9dca34f3fd82
jordiindahood <hazemouni123@gmail.com>
2024-12-02 23:20:34 +0100

    task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+module file for howdy_partner
+
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """a function that concats two arrays
+
+    Keyword arguments:
+    arr1 -- list of int
+    arr2 -- list of int
+    Return: list
+    """
+    return arr1 + arr2
