ccf8753c70a3a4989ab4e77d9beb67aaa7cf8a76
jordiindahood <hazemouni123@gmail.com>
2024-12-05 00:14:02 +0100

    task 8

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+module file for riding_bareback
+
+"""
+
+
+def mat_mul(mat1, mat2):
+    """a function that performs matrix multiplication
+
+    Keyword arguments:
+    mat1 -- matrix
+    mat2 -- matrix
+    Return: matrix
+
+    """
+
+    rows1 = len(mat1)  # 3
+    cols1 = len(mat1[0])  # 2
+    rows2 = len(mat2)  # 2
+    cols2 = len(mat2[0])  # 4
+
+    if cols1 != rows2:
+        raise None
+
+    result = [[0 for idx in range(cols2)] for idx in range(rows1)]
+
+    for i in range(rows1):
+        for j in range(cols2):
+            for k in range(cols1):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+
+    return result
