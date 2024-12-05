15dc89a1efac26bd6b73c09c15f2ea3914e4e526
jordiindahood <hazemouni123@gmail.com>
2024-12-05 15:58:14 +0100

    task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+module file for cats_got_your_tongue
+
+"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """a function that multiplies two matrices
+
+    Keyword arguments:
+    mat1 -- numpy.ndarray
+    mat2 -- numpy.ndarray
+    Return -- numpy.ndarray
+    """
+    return np.matmul(mat1, mat2)
