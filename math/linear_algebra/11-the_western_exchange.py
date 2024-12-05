4b2665675b20d2d6eacf5bb3a31692c6d55f9e5d
jordiindahood <hazemouni123@gmail.com>
2024-12-05 15:06:08 +0100

    task 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+module file for the_western_exchange
+
+"""
+
+
+def np_transpose(matrix):
+    """a function that transposes matrix
+
+    Keyword arguments:
+    matrix -- numpy.ndarray:
+    Return: numpy.ndarray
+
+    """
+    return matrix.transpose()
