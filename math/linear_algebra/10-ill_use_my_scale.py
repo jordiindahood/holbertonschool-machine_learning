140b9816fcc9cb471f83e66d1ec3de9dd0b17d18
jordiindahood <hazemouni123@gmail.com>
2024-12-05 14:57:55 +0100

    task 10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+module file for ill_use_my_scale
+
+"""
+
+
+def np_shape(matrix):
+    """a function that calculates the shape of a numpy.ndarray
+
+    Keyword arguments:
+    matrix -- numpy.ndarray:
+    Return: tuple of int
+
+    """
+    return matrix.shape
