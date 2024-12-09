d5f32ca5f77fdc53d4d6a5a1b9a679afcf393504
jordiindahood <hazemouni123@gmail.com>
2024-12-09 17:57:03 +0100

    task 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -6,13 +6,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
     """
-    display y = x^3 
+    display y = x^3
     """
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(np.arange(0, 11), y, 'r')
+    plt.plot(np.arange(0, 11), y, "r")
     plt.xlim((0, 10))
     plt.show()
