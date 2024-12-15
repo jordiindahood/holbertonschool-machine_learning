f28074e2e1e186d78a7e85f37abbc3e279391104
jordiindahood <hazemouni123@gmail.com>
2024-12-15 23:16:57 +0100

    task 17

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+    matisse
+"""
+
+
+def poly_integral(poly, C=0):
+    """
+    calculation
+    """
+    if not isinstance(poly, list) or poly == []:
+        return None
+
+    if not isinstance(C, (int, float)):
+        return None
+
+    dx = [C]
+    
+    if len(poly) != 1:
+        for pow, coef in enumerate(poly):
+            new_coef = coef / (pow + 1)
+            
+            dx.append(int(new_coef) if new_coef.is_integer() else new_coef)
+
+        while len(dx) > 1 and dx[-1] == 0:
+            dx.pop()
+
+    return dx
