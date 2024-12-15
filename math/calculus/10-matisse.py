d2f2d01c21c87aea6e5f3bdfe35ad06628b0b96e
jordiindahood <hazemouni123@gmail.com>
2024-12-15 21:02:00 +0100

    task 10

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    matisse
+"""
+
+
+def poly_derivative(poly):
+    """
+    calculation
+    """
+    if not isinstance(poly, list) or not all(isinstance(c, int) for c in poly):
+        return None
+
+    if len(poly) == 1:
+        return [0]
+
+    dx = [coeff * poww for poww, coeff in enumerate(poly) if poww > 0]
+
+    return dx
