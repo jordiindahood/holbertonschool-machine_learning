5edfe8949a1b30e909a3d53cb06ede3e9d51fb3b
jordiindahood <hazemouni123@gmail.com>
2024-12-15 20:30:16 +0100

    task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -8,6 +8,6 @@ def summation_i_squared(n):
     """
     calculation
     """
-    if type(n) == int and n > 0:
+    if isinstance(n, int) and n > 0:
         return n * (n + 1) * (2 * n + 1) // 6
     return None
