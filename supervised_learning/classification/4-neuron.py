671dbff490b747db28e519236926a970dab1b7d2
jordiindahood <hazemouni123@gmail.com>
2025-02-05 12:02:35 +0100

    task 4

--- supervised_learning/classification/4-neuron.py
+++ supervised_learning/classification/4-neuron.py
@@ -53,8 +53,8 @@ class Neuron:
         """
         Calculates the cost of the model using logistic regression
         """
-        cost = -np.sum((Y * np.log(A)) + ((
-            1 - Y) * np.log(1.0000001 - A))) / Y.shape[1]
+        cost = -np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
+        cost = cost / Y.shape[1]
         return cost
 
     def evaluate(self, X, Y):
