7f6e6927922c8ebf9fe75cb1a55ee238435ca4af
jordiindahood <hazemouni123@gmail.com>
2025-03-18 07:22:44 +0100

    script 0

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -18,6 +18,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         )
     )
 
+    model.add(K.layers.Dropout(1 - keep_prob))
 
     for i in range(1, len(layers)):
         model.add(K.layers.Dropout(1 - keep_prob))
