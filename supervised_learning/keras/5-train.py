ba5d222249f78772b96b6db8691f8b3662823b4b
jordiindahood <hazemouni123@gmail.com>
2025-03-28 09:29:27 +0100

    script 5

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" script 4"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent with optional validation.
+    """
+    history = network.fit(data,
+                          labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          validation_data=validation_data,
+                          verbose=verbose,
+                          shuffle=shuffle
+                          )
+
+    return history
