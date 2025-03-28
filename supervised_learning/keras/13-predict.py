c05d7ec062b211f0115d7dd4aab3193fbe6db06f
jordiindahood <hazemouni123@gmail.com>
2025-03-28 10:58:00 +0100

    script 13

--- supervised_learning/keras/13-predict.py
+++ supervised_learning/keras/13-predict.py
@@ -6,12 +6,5 @@ import tensorflow.keras as K
 def predict(network, data, verbose=False):
     """
     Makes a prediction using a neural network.
-
-    Args:
-        network (keras.Model): The neural network model to use for prediction.
-        data (numpy.ndarray): The input data to make predictions on.
-        verbose (bool): Whether to print output during the prediction process.
-
-    Returns:
     """
     return network.predict(data, verbose=verbose)
