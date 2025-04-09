a93829439a342ce7538b2c22ebb719b94c4df5fe
jordiindahood <hazemouni123@gmail.com>
2025-04-09 17:36:10 +0100

    script 5

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" script 5"""
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    Updates a variable using the gradient descent with
+    momentum optimization algorithm.
+    """
+    v = beta1 * v + (1 - beta1) * grad
+    var = var - alpha * v
+    return var, v
