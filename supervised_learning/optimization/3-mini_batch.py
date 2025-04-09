5422a15c72431e781811659cf279d8d42b973b62
jordiindahood <hazemouni123@gmail.com>
2025-04-09 17:13:05 +0100

    script 3

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ script 3 """
 
-shuffle_data = __import__("2-shuffle_data").shuffle_data
+shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 def create_mini_batches(X, Y, batch_size):
     """
