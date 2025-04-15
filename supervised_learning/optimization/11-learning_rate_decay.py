67e7c1617ac04e85a394eccdef7c27ee42212db0
jordiindahood <hazemouni123@gmail.com>
2025-04-15 16:32:57 +0100

    script 11

---
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" script 11 """
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    Updates the learning rate using inverse time decay in NumPy.
+    """
+
+    decayed= alpha / (1 + decay_rate * int(global_step / decay_step))
+    return (decayed)
