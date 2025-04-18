3e60e293c85a01157f6555325793a5783e4019a1
jordiindahood <hazemouni123@gmail.com>
2025-04-18 15:57:06 +0100

    script 0

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -7,11 +7,11 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     """
     Calculates the cost of a neural network with L2 regularization.
     """
-    summation = 0
+    sum = 0
     for idx in range(1, L + 1):
         key = "W{}".format(idx)
-        summation += np.linalg.norm(weights[key])
+        sum += np.linalg.norm(weights[key])
 
-    L2_cost = lambtha * summation / (2 * m)
+    L2_cost = lambtha * sum / (2 * m)
 
     return cost + L2_cost
