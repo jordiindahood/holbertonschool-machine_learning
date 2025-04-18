e04e3b0040adb63676335bf5c14c8f494bd91d46
jordiindahood <hazemouni123@gmail.com>
2025-04-18 12:46:25 +0100

    script 3

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ script 3 """
-shuffle_data = __import__("2-shuffle_data").shuffle_data
+
+shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
 def create_mini_batches(X, Y, batch_size):
@@ -13,7 +14,7 @@ def create_mini_batches(X, Y, batch_size):
     if batch_size > m:
         batch_size = m
     for i in range(0, m, batch_size):
-        X_batch = x_shuffled[i : i + batch_size]
-        Y_batch = y_shuffled[i : i + batch_size]
+        X_batch = x_shuffled[i: i + batch_size]
+        Y_batch = y_shuffled[i: i + batch_size]
         mini_batches.append((X_batch, Y_batch))
     return mini_batches
