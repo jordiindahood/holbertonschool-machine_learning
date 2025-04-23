9c2ee539a22c6d7536b0da6e40a10d04c2b781af
jordiindahood <hazemouni123@gmail.com>
2025-04-23 16:08:30 +0100

    script 4

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ script 4 """
-import tensorflow.compat.v1 as tf # type: ignore
+import tensorflow.compat.v1 as tf  # type: ignore
 
 
 def lenet5(x, y):
@@ -13,52 +13,53 @@ def lenet5(x, y):
 
     # Layer 1: Conv -> ReLU
     conv1 = tf.compat.v1.layers.conv2d(
-        inputs=x, filters=6, kernel_size=5, padding='same',
-        activation=tf.nn.relu, kernel_initializer=he_init
+        inputs=x,
+        filters=6,
+        kernel_size=5,
+        padding="same",
+        activation=tf.nn.relu,
+        kernel_initializer=he_init,
     )
 
     # Layer 2: Max Pooling
-    pool1 = tf.compat.v1.layers.max_pooling2d(
-        inputs=conv1, pool_size=2, strides=2
-    )
+    pool1 = tf.compat.v1.layers.max_pooling2d(inputs=conv1, pool_size=2, strides=2)
 
     # Layer 3: Conv -> ReLU
     conv2 = tf.compat.v1.layers.conv2d(
-        inputs=pool1, filters=16, kernel_size=5, padding='valid',
-        activation=tf.nn.relu, kernel_initializer=he_init
+        inputs=pool1,
+        filters=16,
+        kernel_size=5,
+        padding="valid",
+        activation=tf.nn.relu,
+        kernel_initializer=he_init,
     )
 
     # Layer 4: Max Pooling
-    pool2 = tf.compat.v1.layers.max_pooling2d(
-        inputs=conv2, pool_size=2, strides=2
-    )
+    pool2 = tf.compat.v1.layers.max_pooling2d(inputs=conv2, pool_size=2, strides=2)
 
     # Flatten
     flat = tf.compat.v1.layers.flatten(pool2)
 
     # Layer 5: Fully Connected -> ReLU
     fc1 = tf.compat.v1.layers.dense(
-        inputs=flat, units=120,
-        activation=tf.nn.relu, kernel_initializer=he_init
+        inputs=flat, units=120, activation=tf.nn.relu, kernel_initializer=he_init
     )
 
     # Layer 6: Fully Connected -> ReLU
     fc2 = tf.compat.v1.layers.dense(
-        inputs=fc1, units=84,
-        activation=tf.nn.relu, kernel_initializer=he_init
+        inputs=fc1, units=84, activation=tf.nn.relu, kernel_initializer=he_init
     )
 
     # Output Layer: Fully Connected -> Softmax
-    logits = tf.compat.v1.layers.dense(
-        inputs=fc2, units=10,
-        kernel_initializer=he_init
-    )
+    logits = tf.compat.v1.layers.dense(inputs=fc2, units=10, kernel_initializer=he_init)
 
     # Softmax output
     y_pred = tf.nn.softmax(logits)
 
     # Loss: softmax cross-entropy
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+    loss = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y)
+    )
 
     # Optimizer: Adam
     train_op = tf.compat.v1.train.AdamOptimizer().minimize(loss)
