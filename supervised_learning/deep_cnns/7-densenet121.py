9fe1db4389c1806fa717b2535452ca6b1bef87bd
jordiindahood <hazemouni123@gmail.com>
2025-04-25 15:51:48 +0100

    script 7

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -19,36 +19,35 @@ def densenet121(growth_rate=32, compression=1.0):
     my_layer = K.layers.BatchNormalization(axis=3)(X)
     my_layer = K.layers.ReLU()(my_layer)
 
-    my_layer = K.layers.Conv2D(
-        filters=2 * growth_rate,
-        kernel_size=(7, 7),
-        strides=(2, 2),
-        padding='same',
-        kernel_initializer=initializer,
-    )(my_layer)
+    my_layer = K.layers.Conv2D(filters=2 * growth_rate,
+                               kernel_size=(7, 7),
+                               strides=(2, 2),
+                               padding='same',
+                               kernel_initializer=initializer)(my_layer)
 
-    my_layer = K.layers.MaxPooling2D(
-        pool_size=(3, 3), padding='same', strides=(2, 2)
-    )(my_layer)
+    my_layer = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                     padding='same',
+                                     strides=(2, 2))(my_layer)
 
     nb_filters = 2 * growth_rate
 
     my_layer, nb_filters = dense_block(my_layer, nb_filters, growth_rate, 6)
 
     for layer in layers:
-        my_layer, nb_filters = transition_layer(
-            my_layer, nb_filters, compression
-        )
+        my_layer, nb_filters = transition_layer(my_layer,
+                                                nb_filters,
+                                                compression)
 
-        my_layer, nb_filters = dense_block(
-            my_layer, nb_filters, growth_rate, layer
-        )
+        my_layer, nb_filters = dense_block(my_layer,
+                                           nb_filters,
+                                           growth_rate,
+                                           layer)
 
     my_layer = K.layers.AveragePooling2D(pool_size=(7, 7))(my_layer)
 
-    my_layer = K.layers.Dense(
-        units=1000, activation='softmax', kernel_initializer=initializer
-    )(my_layer)
+    my_layer = K.layers.Dense(units=1000,
+                              activation='softmax',
+                              kernel_initializer=initializer)(my_layer)
 
     model = K.models.Model(inputs=X, outputs=my_layer)
 
