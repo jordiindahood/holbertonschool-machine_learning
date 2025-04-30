1d84d14ef1244c8936acb090d16de2bf6382815f
jordiindahoood <hazem.ouni123@gmail.com>
2025-04-30 14:27:13 +0100

    script 2

---
+++ supervised_learning/object_detection/2-yolo.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+
+"""script 1"""
+
+import tensorflow.keras as K
+import numpy as np
+
+
+class Yolo:
+    """
+    The Yolo class is used for object detection using the YOLOv3 model.
+    It initializes with the necessary configurations and loads
+    the pre-trained model.
+    """
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """
+        Initializes the Yolo class with the provided model,
+        class names, and thresholds.
+        """
+        self.model = K.models.load_model(model_path)
+        with open(classes_path, "r") as f:
+            self.class_names = [line.strip() for line in f]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def sigmoid_f(self, x):
+        """
+        Apply the sigmoid activation function.
+        """
+        return 1 / (1 + np.exp(-x))
+
+    def process_outputs(self, outputs, image_size):
+        """
+        Process the outputs from the YOLO model.
+        """
+
+        boxes = []
+        for i in range(len(outputs)):
+            boxes_i = outputs[i][..., 0:4]
+            grid_h_i = outputs[i].shape[0]
+            grid_w_i = outputs[i].shape[1]
+            anchor_box_i = outputs[i].shape[2]
+
+            for anchor_n in range(anchor_box_i):
+                for cy_n in range(grid_h_i):
+                    for cx_n in range(grid_w_i):
+
+                        tx_n = outputs[i][cy_n, cx_n, anchor_n, 0:1]
+                        ty_n = outputs[i][cy_n, cx_n, anchor_n, 1:2]
+                        tw_n = outputs[i][cy_n, cx_n, anchor_n, 2:3]
+                        th_n = outputs[i][cy_n, cx_n, anchor_n, 3:4]
+
+                        # size of the anchors
+                        pw_n = self.anchors[i][anchor_n][0]
+                        ph_n = self.anchors[i][anchor_n][1]
+
+                        # calculating center
+                        bx_n = self.sigmoid_f(tx_n) + cx_n
+                        by_n = self.sigmoid_f(ty_n) + cy_n
+
+                        # calculating hight and width
+                        bw_n = pw_n * np.exp(tw_n)
+                        bh_n = ph_n * np.exp(th_n)
+
+                        # generating new center
+                        new_bx_n = bx_n / grid_w_i
+                        new_by_n = by_n / grid_h_i
+
+                        # generating new hight and width
+                        new_bh_n = bh_n / int(self.model.input.shape[2])
+                        new_bw_n = bw_n / int(self.model.input.shape[1])
+
+                        # calculating (cx1, cy1) and (cx2, cy2) coords
+                        y1 = (new_by_n - (new_bh_n / 2)) * image_size[0]
+                        y2 = (new_by_n + (new_bh_n / 2)) * image_size[0]
+                        x1 = (new_bx_n - (new_bw_n / 2)) * image_size[1]
+                        x2 = (new_bx_n + (new_bw_n / 2)) * image_size[1]
+
+                        boxes_i[cy_n, cx_n, anchor_n, 0] = x1
+                        boxes_i[cy_n, cx_n, anchor_n, 1] = y1
+                        boxes_i[cy_n, cx_n, anchor_n, 2] = x2
+                        boxes_i[cy_n, cx_n, anchor_n, 3] = y2
+
+            boxes.append(boxes_i)
+
+        # 2. box confidence = dim [4]
+        confidence = []
+        for i in range(len(outputs)):
+            confidence_i = self.sigmoid_f(outputs[i][..., 4:5])
+            confidence.append(confidence_i)
+
+        # 3. box class_probs = dim [5:]
+        probs = []
+        for i in range(len(outputs)):
+            probs_i = self.sigmoid_f(outputs[i][:, :, :, 5:])
+            probs.append(probs_i)
+
+        return (boxes, confidence, probs)
+
+    def filter_boxes(self, boxes, box_confidences, box_class_probs):
+        """
+        Filter and process bounding boxes based on class confidence scores.
+        """
+        scores = []
+        classes = []
+        box_classes_scores = []
+        index_arg_max = []
+        box_classes = []
+
+        for bc_i, probs_j in zip(box_confidences, box_class_probs):
+            scores.append(bc_i * probs_j)
+
+        for score in scores:
+            index_arg_max = np.argmax(score, axis=-1)
+
+            index_arg_max_flat = index_arg_max.flatten()
+
+            classes.append(index_arg_max_flat)
+
+            score_max = np.max(score, axis=-1)
+            score_max_flat = score_max.flatten()
+            box_classes_scores.append(score_max_flat)
+
+        boxes = [box.reshape(-1, 4) for box in boxes]
+
+        box_classes = np.concatenate(classes, axis=-1)
+        box_classes_scores = np.concatenate(box_classes_scores, axis=-1)
+
+        boxes = np.concatenate(boxes, axis=0)
+
+        filtro = np.where(box_classes_scores >= self.class_t)
+
+        return (boxes[filtro], box_classes[filtro], box_classes_scores[filtro])
