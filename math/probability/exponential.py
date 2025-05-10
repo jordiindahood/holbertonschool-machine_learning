9588d44125bfe57aac47c8ffce26a5a5d164cec2
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-10 16:29:03 +0100

    sccript exponential

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -26,3 +26,9 @@ class Exponential:
         if x < 0:
             return 0
         return self.lambtha * self.e**(-self.lambtha * x)
+
+    def cdf(self, x):
+        """Calculates the CDF for a given time period x"""
+        if x < 0:
+            return 0
+        return 1 - self.e ** (-self.lambtha * x)
