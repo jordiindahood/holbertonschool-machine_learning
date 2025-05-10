594e798d8f33f8ef06b4becd2e04e8681975f8a4
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-10 17:21:10 +0100

    script 0

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
+"""script 0"""
+
 import numpy as np
 
+
 def likelihood(x, n, P):
-    """Calculates the likelihood of obtaining data given various probabilities"""
-    
-    # Validate input parameters
+    """likelihood"""
+
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
@@ -17,7 +21,10 @@ def likelihood(x, n, P):
     if np.any((P < 0) | (P > 1)):
         raise ValueError("All values in P must be in the range [0, 1]")
 
-    coeff = np.math.comb(n, x)
-    likelihood_values = coeff * (P ** x) * ((1 - P) ** (n - x))
-    
-    return likelihood_values
+    # Use binomial coefficient
+    from math import comb
+
+    coeff = comb(n, x)
+    likelihood_values = coeff * (P**x) * ((1 - P) ** (n - x))
+
+    return np.array(likelihood_values)
