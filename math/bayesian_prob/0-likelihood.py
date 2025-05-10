967d2df86cd6831b52ba67084340c38cafff860a
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-10 17:27:45 +0100

    script 0

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -3,26 +3,38 @@
 """script 0"""
 
 import numpy as np
-from math import comb
+
+def n_choose_x(n, x):
+    """n_choose_x"""
+    n_fact = np.math.factorial(n)
+    x_fact = np.math.factorial(x)
+    nx_fact = np.math.factorial(n - x)
+    return n_fact / (x_fact * nx_fact)
 
 
 def likelihood(x, n, P):
     """likelihood"""
 
-    if not isinstance(n, int) or n <= 0:
+    if (not isinstance(n, (int, float)) or n <= 0):
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x < 0:
-        raise ValueError(
-            "x must be an integer that is greater than or equal to 0"
-        )
-    if x > n:
+
+    if (not isinstance(x, (int, float)) or x < 0):
+        message = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(message)
+
+    if (x > n):
         raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) or P.ndim != 1:
+
+    if (not isinstance(P, np.ndarray) or len(P.shape) != 1 or P.shape[0] < 1):
         raise TypeError("P must be a 1D numpy.ndarray")
-    if np.any((P < 0) | (P > 1)):
+
+    if (np.any(P > 1) or np.any(P < 0)):
         raise ValueError("All values in P must be in the range [0, 1]")
 
-    coeff = comb(n, x)
-    likelihood_values = coeff * (P**x) * ((1 - P) ** (n - x))
+    binomial_coef = n_choose_x(n, x)
+    success_rate = pow(P, x)
+    failure_rate = pow(1 - P, n - x)
 
-    return np.array(likelihood_values)
+    likelihood = binomial_coef * success_rate * failure_rate
+
+    return likelihood
