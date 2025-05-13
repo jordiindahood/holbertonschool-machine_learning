ae2a1d60f8e49e48550e02f0a858bab9dc70831d
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-13 16:03:07 +0100

    script 1

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -8,21 +8,11 @@ def pca(X, ndim):
     """
     Performs PCA on a dataset
     """
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
-        raise TypeError("X must be a 2D numpy.ndarray")
-    if not isinstance(ndim, int) or ndim <= 0 or ndim > X.shape[1]:
-        raise ValueError(
-            "ndim must be a positive integer less \
-            than or equal to the number of features"
-        )
+    X_m = X - np.mean(X, axis=0)
 
-    X_mean = np.mean(X, axis=0)
-    X_centered = X - X_mean
+    u, s, vh = np.linalg.svd(X_m)
 
-    U, S, Vt = np.linalg.svd(X_centered, full_matrices=False)
-
-    W = Vt[:ndim].T
-
-    T = np.dot(X_centered, W)
+    W = vh[:ndim].T
+    T = np.matmul(X_m, W)
 
     return T
