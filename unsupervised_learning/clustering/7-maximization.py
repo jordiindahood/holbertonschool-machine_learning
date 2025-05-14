f54042d79d9099f2bd26682bcd8305912518ccda
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-14 19:14:25 +0100

    script 7

---
+++ unsupervised_learning/clustering/7-maximization.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import numpy as np
+
+def maximization(X, g):
+    """Performs the maximization step in the EM algorithm for a GMM"""
+    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
+        not isinstance(g, np.ndarray) or g.ndim != 2):
+        return None, None, None
+
+    n, d = X.shape
+    k, n_g = g.shape
+
+    if n != n_g:
+        return None, None, None
+
+    try:
+        N_k = np.sum(g, axis=1)
+
+        pi = N_k / n
+
+        m = (g @ X) / N_k[:, np.newaxis]
+
+        S = np.empty((k, d, d))
+        for i in range(k):
+            X_centered = X - m[i]
+            weighted = g[i][:, np.newaxis] * X_centered
+            S[i] = (weighted.T @ X_centered) / N_k[i]
+
+        return pi, m, S
+    except Exception:
+        return None, None, None
