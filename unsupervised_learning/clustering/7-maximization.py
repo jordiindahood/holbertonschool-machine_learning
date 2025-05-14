64021783e4ce6f1e2902cc065d3356e4c221c2f8
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-15 00:22:36 +0100

    script 7

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -7,32 +7,32 @@ import numpy as np
 
 def maximization(X, g):
     """Performs the maximization step in the EM algorithm for a GMM"""
+
     if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None, None
+
     if type(g) is not np.ndarray or len(g.shape) != 2:
         return None, None, None
+
     if X.shape[0] != g.shape[1]:
         return None, None, None
 
+    cluster = np.sum(g, axis=0)
+    cluster = np.sum(cluster)
+
+    if int(cluster) != X.shape[0]:
+        return None, None, None
+
     n, d = X.shape
-    k, n_g = g.shape
+    k, n = g.shape
+    nk = np.sum(g, axis=1)
+    pi = nk / n
+    mean = np.zeros((k, d))
+    cov = np.zeros((k, d, d))
 
-    if n != n_g:
-        return None, None, None
+    for i in range(k):
+        mean[i] = np.matmul(g[i], X) / nk[i]
+        norm = X - mean[i]
+        cov[i] = np.matmul(g[i] * norm.T, norm) / nk[i]
 
-    try:
-        N_k = np.sum(g, axis=1)
-
-        pi = N_k / n
-
-        m = (g @ X) / N_k[:, np.newaxis]
-
-        S = np.empty((k, d, d))
-        for i in range(k):
-            X_centered = X - m[i]
-            weighted = g[i][:, np.newaxis] * X_centered
-            S[i] = (weighted.T @ X_centered) / N_k[i]
-
-        return pi, m, S
-    except Exception:
-        return None, None, None
+    return pi, mean, cov
