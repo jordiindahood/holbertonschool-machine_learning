27a44dcf9ed2f0f9a2dbb679624532b691b32594
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-16 13:15:58 +0100

    script 10

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """script 10"""
 
-from sklearn.cluster
+import sklearn.cluster
 
 
 def kmeans(X, k):
     """
     Performs K-means clustering on a dataset.
     """
-    model = cluster.KMeans(n_clusters=k, n_init='auto')
+    model = sklearn.cluster.KMeans(n_clusters=k, n_init='auto')
     model.fit(X)
 
     C = model.cluster_centers_
