4343cdd18af68bdd63b5924d1e56232e455f6187
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-16 13:13:39 +0100

    script 10

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """script 10"""
-import numpy as np
+
 from sklearn.cluster import KMeans
 
 
@@ -8,11 +8,6 @@ def kmeans(X, k):
     """
     Performs K-means clustering on a dataset.
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None, None
-    if not isinstance(k, int) or k <= 0:
-        return None, None
-
     model = KMeans(n_clusters=k, n_init='auto')
     model.fit(X)
 
