08bb5e7f55bb5d09f8606a40f222a95f72c93300
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-20 14:59:08 +0100

    script 2

---
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""script 2"""
+
+from gensim.models import Word2Vec
+
+
+def word2vec_model(
+    sentences,
+    vector_size=100,
+    min_count=5,
+    window=5,
+    negative=5,
+    cbow=True,
+    epochs=5,
+    seed=0,
+    workers=1,
+):
+    """
+    Trains a Word2Vec model on the given sentences.
+    """
+    sg = 0 if cbow else 1
+
+    model = Word2Vec(
+        sentences=sentences,
+        vector_size=vector_size,
+        window=window,
+        min_count=min_count,
+        sg=sg,
+        negative=negative,
+        seed=seed,
+        workers=workers,
+    )
+
+    model.train(sentences, total_examples=len(sentences), epochs=epochs)
+
+    return model
