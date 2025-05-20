173190848d1893c09c60959f369282e0b8837e32
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-20 14:20:57 +0100

    script 0

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """script 0"""
 
-import numpy as np
-import re
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def bag_of_words(sentences, vocab=None):
@@ -10,28 +9,9 @@ def bag_of_words(sentences, vocab=None):
     Creates a bag-of-words embedding matrix
     """
 
-    def tokenize(sentence):
-        return re.findall(r'\b\w+\b', sentence.lower())
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    x = vectorizer.fit_transform(sentences)
+    embedding = x.toarray()
+    features = vectorizer.get_feature_names_out()
 
-    tokenized = [tokenize(sent) for sent in sentences]
-
-    if vocab is None:
-        unique_words = set()
-        for tokens in tokenized:
-            unique_words.update(tokens)
-        features = sorted(unique_words)
-    else:
-        features = sorted(set(vocab))
-
-    word_to_index = {word: i for i, word in enumerate(features)}
-
-    s = len(sentences)
-    f = len(features)
-    embeddings = np.zeros((s, f), dtype=int)
-
-    for i, tokens in enumerate(tokenized):
-        for word in tokens:
-            if word in word_to_index:
-                embeddings[i, word_to_index[word]] += 1
-
-    return embeddings, features
+    return embedding, features
