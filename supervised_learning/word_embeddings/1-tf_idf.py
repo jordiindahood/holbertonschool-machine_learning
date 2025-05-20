2d15903945eb72ce1e762e462d5d89ce8213f877
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-20 14:52:15 +0100

    script 1

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -9,15 +9,10 @@ def tf_idf(sentences, vocab=None):
     Creates a TF-IDF embedding matrix using sklearn.
     """
 
-    def preprocess(sentence):
-        return ' '.join(re.findall(r'\b\w+\b', sentence.lower()))
-
-    cleaned_sentences = [preprocess(s) for s in sentences]
-
     vectorizer = TfidfVectorizer(
         vocabulary=sorted(set(vocab)) if vocab else None
     )
-    embeddings = vectorizer.fit_transform(cleaned_sentences).toarray()
+    embeddings = vectorizer.fit_transform(sentences).toarray()
     features = vectorizer.get_feature_names_out().tolist()
 
     return embeddings, features
