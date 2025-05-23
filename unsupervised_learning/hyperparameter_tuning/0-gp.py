8f49dc857ff090e2a5ee1951f3a16950a9f91dbb
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-23 12:06:12 +0100

    script 0

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -16,15 +16,23 @@ class GaussianProcess:
         self.Y = Y_init
         self.l = l
         self.sigma_f = sigma_f
-        self.K = self.kernel(self.X, self.X)
+
+        self.K = self.kernel(X_init, X_init)
 
     def kernel(self, X1, X2):
         """
-        Computes the covariance kernel matrix using the RBF kernel
+        Calculates the covariance kernel matrix between two matrices
+        using the Radial Basis Function (RBF).
         """
-        sqdist = (
-            np.sum(X1**2, axis=1).reshape(-1, 1)
-            + np.sum(X2**2, axis=1)
-            - 2 * np.dot(X1, X2.T)
-        )
-        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+        # K(xᵢ, xⱼ) = σ² exp((-0.5 / 2l²)(xᵢ − xⱼ)ᵀ (xᵢ − xⱼ))
+        σ2 = self.sigma_f**2
+        l2 = self.l**2
+
+        sqr_sumx1 = np.sum(X1**2, 1).reshape(-1, 1)
+        # print("sqr_sum1", sqr_sumx1)
+        sqr_sumx2 = np.sum(X2**2, 1)
+        # print("sqr_sum2", sqr_sumx2)
+        sqr_dist = sqr_sumx1 - 2 * np.dot(X1, X2.T) + sqr_sumx2
+
+        kernel = σ2 * np.exp(-0.5 / l2 * sqr_dist)
+        return kernel
