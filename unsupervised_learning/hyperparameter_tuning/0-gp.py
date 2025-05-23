58927dd9ad60c23001a0a01f69c543412ebef938
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-23 11:49:24 +0100

    script 0

---
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""script 0"""
+
+import numpy as np
+
+
+class GaussianProcess:
+    """Represents a noiseless 1D Gaussian process"""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        Constructor for the GaussianProcess class
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """
+        Computes the covariance kernel matrix using the RBF kernel
+        """
+        sqdist = np.sum(X1**2, axis=1).reshape(-1, 1) \
+               + np.sum(X2**2, axis=1) \
+               - 2 * np.dot(X1, X2.T)
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
