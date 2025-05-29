e4a61183d84a04e7dcb705b0e5b49aa1428780d4
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-29 16:13:45 +0100

    script 2

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -1,50 +1,55 @@
 #!/usr/bin/env python3
-"""RNN Decoder with attention"""
+"""RNN Decoder with Self-Attention"""
 
 import tensorflow as tf
 SelfAttention = __import__('1-self_attention').SelfAttention
 
 
 class RNNDecoder(tf.keras.layers.Layer):
-    """Decoder for sequence-to-sequence with attention"""
+    """RNN Decoder class"""
 
     def __init__(self, vocab, embedding, units, batch):
-        """Constructor for RNNDecoder"""
+        """Initialize the RNNDecoder"""
         super(RNNDecoder, self).__init__()
         self.units = units
-        self.batch = batch
 
         self.embedding = tf.keras.layers.Embedding(
-            input_dim=vocab, output_dim=embedding
+            input_dim=vocab,
+            output_dim=embedding
         )
 
         self.gru = tf.keras.layers.GRU(
-            units,
+            units=units,
             return_sequences=True,
             return_state=True,
             recurrent_initializer='glorot_uniform'
         )
 
         self.F = tf.keras.layers.Dense(vocab)
+
         self.attention = SelfAttention(units)
 
     def call(self, x, s_prev, hidden_states):
-        """Performs the forward pass"""
-        # Apply attention
+        """Forward pass"""
+        # Compute context vector from attention mechanism
         context, _ = self.attention(s_prev, hidden_states)  # (batch, units)
 
-        # Embed the input word
+        # Embed input token
         x = self.embedding(x)  # (batch, 1, embedding)
 
-        # Concatenate context vector with input
-        context = tf.expand_dims(context, 1)  # (batch, 1, units)
+        # Add time dimension to context vector
+        context = tf.expand_dims(context, axis=1)  # (batch, 1, units)
+
+        # Concatenate context and embedded input
         x = tf.concat([context, x], axis=-1)  # (batch, 1, units + embedding)
 
         # Pass through GRU
         output, state = self.gru(x, initial_state=s_prev)  # output: (batch, 1, units)
 
-        # Project output to vocabulary
-        output = tf.reshape(output, (-1, output.shape[2]))  # (batch, units)
+        # Remove time dimension from GRU output
+        output = output[:, 0, :]  # (batch, units)
+
+        # Final output logits
         y = self.F(output)  # (batch, vocab)
 
         return y, state
