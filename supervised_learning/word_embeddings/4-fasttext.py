47b96ddce6be6b49ec99d5ded3605b2e5fa19157
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-29 10:27:31 +0100

    script 4

---
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""script 4"""
+import gensim
+
+
+def fasttext_model(
+    sentences,
+    vector_size=100,
+    min_count=5,
+    negative=5,
+    window=5,
+    cbow=True,
+    epochs=5,
+    seed=0,
+    workers=1,
+):
+    """
+    Trains a FastText model on the given sentences.
+    """
+    sg = 0 if cbow else 1
+
+    model = gensim.models.FastText(
+        vector_size=vector_size,
+        window=window,
+        min_count=min_count,
+        sg=sg,
+        negative=negative,
+        seed=seed,
+        workers=workers,
+    )
+
+    model.build_vocab(sentences)
+    model.train(sentences, total_examples=model.corpus_count, epochs=epochs)
+
+    return model
