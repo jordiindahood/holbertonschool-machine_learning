2d6f0c4abbdaa765c454edd4fbdbe7de169df292
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-29 14:09:53 +0100

    script 1

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -6,17 +6,23 @@ import tensorflow as tf
 
 
 class SelfAttention(tf.keras.layers.Layer):
-    """SelfAttention Class"""
+    """
+    SelfAttention Class
+    """
 
     def __init__(self, units):
-        """init"""
+        """
+        init
+        """
         super(SelfAttention, self).__init__()
         self.W = tf.keras.layers.Dense(units)
         self.U = tf.keras.layers.Dense(units)
         self.V = tf.keras.layers.Dense(1)
 
     def call(self, s_prev, hidden_states):
-        """call"""
+        """
+        call
+        """
         s_prev_expanded = tf.expand_dims(s_prev, 1)
 
         score = self.V(
