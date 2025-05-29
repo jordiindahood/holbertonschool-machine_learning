34b1d1bc570ba41c1d9f15a94ab33f0b9991ad61
jordiindahoood <hazem.ouni123@gmail.com>
2025-05-29 16:09:42 +0100

    script 2

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-
-"""script 2"""
+"""RNN Decoder with attention"""
 
 import tensorflow as tf
-
 SelfAttention = __import__('1-self_attention').SelfAttention
 
 
 class RNNDecoder(tf.keras.layers.Layer):
-    """rnn decoder class"""
+    """Decoder for sequence-to-sequence with attention"""
 
     def __init__(self, vocab, embedding, units, batch):
-        """init"""
+        """Constructor for RNNDecoder"""
         super(RNNDecoder, self).__init__()
         self.units = units
         self.batch = batch
@@ -24,22 +22,29 @@ class RNNDecoder(tf.keras.layers.Layer):
             units,
             return_sequences=True,
             return_state=True,
-            recurrent_initializer='glorot_uniform',
+            recurrent_initializer='glorot_uniform'
         )
 
         self.F = tf.keras.layers.Dense(vocab)
-
         self.attention = SelfAttention(units)
 
     def call(self, x, s_prev, hidden_states):
-        """call"""
-        context, _ = self.attention(s_prev, hidden_states)
+        """Performs the forward pass"""
+        # Apply attention
+        context, _ = self.attention(s_prev, hidden_states)  # (batch, units)
 
-        x = self.embedding(x)
-        context = tf.expand_dims(context, 1)
-        x = tf.concat([context, x], axis=-1)
-        output, state = self.gru(x, initial_state=s_prev)
+        # Embed the input word
+        x = self.embedding(x)  # (batch, 1, embedding)
 
-        y = self.F(tf.squeeze(output, axis=1))
+        # Concatenate context vector with input
+        context = tf.expand_dims(context, 1)  # (batch, 1, units)
+        x = tf.concat([context, x], axis=-1)  # (batch, 1, units + embedding)
+
+        # Pass through GRU
+        output, state = self.gru(x, initial_state=s_prev)  # output: (batch, 1, units)
+
+        # Project output to vocabulary
+        output = tf.reshape(output, (-1, output.shape[2]))  # (batch, units)
+        y = self.F(output)  # (batch, vocab)
 
         return y, state
