6a1245139530d754067bd8c093baed7fa5bd7074
jordiindahoood <hazem.ouni123@gmail.com>
2025-06-09 00:29:58 +0100

    script 0

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -4,7 +4,7 @@ script 0
 """
 
 import tensorflow_datasets as tfds
-from transformers import BertTokenizerFast
+import transformers
 
 
 class Dataset:
@@ -12,19 +12,25 @@ class Dataset:
 
     def __init__(self):
         """init"""
-        examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
-                                       with_info=True,
-                                       as_supervised=True)
+        examples, metadata = tfds.load(
+            'ted_hrlr_translate/pt_to_en', with_info=True, as_supervised=True
+        )
 
         self.data_train = examples['train']
         self.data_valid = examples['validation']
 
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train
+        )
 
     def tokenize_dataset(self, data):
         """
         Creates sub-word tokenizers for our dataset using pre-trained BERT tokenizers
         """
-        tokenizer_pt = BertTokenizerFast.from_pretrained('neuralmind/bert-base-portuguese-cased')
-        tokenizer_en = BertTokenizerFast.from_pretrained('bert-base-uncased')
+        tokenizer_pt = transformers.BertTokenizerFast.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased'
+        )
+        tokenizer_en = transformers.BertTokenizerFast.from_pretrained(
+            'bert-base-uncased'
+        )
         return tokenizer_pt, tokenizer_en
