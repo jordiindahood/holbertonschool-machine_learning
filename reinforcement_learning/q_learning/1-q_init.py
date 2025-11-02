411f12cbeb9d6568b2fcfdffaf2731617be2944e
jordiindahood <hazem.ouni123@gmail.com>
2025-11-02 19:56:48 +0100

    script 1

--- reinforcement_learning/q_learning/1-q_init.py
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -9,12 +9,6 @@ import numpy as np
 def q_init(env):
     """
     Initializes the Q-table.
-
-    Args:
-        env: the FrozenLakeEnv instance
-
-    Returns:
-        A numpy.ndarray of zeros with shape (number_of_states, number_of_actions)
     """
     n_states = env.observation_space.n
     n_actions = env.action_space.n
