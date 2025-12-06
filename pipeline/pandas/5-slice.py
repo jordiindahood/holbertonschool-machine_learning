3e6e3cfb3a96f9c93026ed763f40cad1d6f143b7
jordiindahood <hazem.ouni123@gmail.com>
2025-12-06 19:09:40 +0100

    script 5

---
+++ pipeline/pandas/5-slice.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" script 5 """
+import pandas as pd
+
+
+def slice(df):
+    """Extracts columns and selects every 60th row."""
+
+    # Select required columns
+    cols = df[["High", "Low", "Close", "Volume_(BTC)"]]
+
+    # Select every 60th row
+    return cols.iloc[::60]
