94df7e7d2afefff6fd2586617de017c4c0dc5871
jordiindahood <hazem.ouni123@gmail.com>
2025-12-06 18:57:55 +0100

    script 4

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" script 4 """
+import numpy as np
+
+
+def array(df):
+    """Selects the last 10 rows of High and Close columns
+    and returns them as a numpy ndarray."""
+
+    # Select last 10 rows from High and Close
+    sub = df[["High", "Close"]].tail(10)
+
+    # Convert to numpy array
+    return sub.to_numpy()
