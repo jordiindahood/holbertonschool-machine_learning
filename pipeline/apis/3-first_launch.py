5a4ab8295dbb9cb0dcbdaf483010d75de40395c3
jordiindahood <hazem.ouni123@gmail.com>
2025-12-06 20:30:49 +0100

    script3

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -5,43 +5,39 @@ import requests
 
 def get_first_launch():
     """
-    Retrieves the first SpaceX launch and prints:
+    Fetches all SpaceX launches and prints the first launch in the format:
     <launch name> (<date>) <rocket name> - <launchpad name> (<launchpad locality>)
     """
-
-    # Get all launches
     launches_url = "https://api.spacexdata.com/v4/launches"
     rockets_url = "https://api.spacexdata.com/v4/rockets/"
     launchpads_url = "https://api.spacexdata.com/v4/launchpads/"
 
-    response = requests.get(launches_url)
-    if response.status_code != 200:
+    # Fetch all launches
+    r = requests.get(launches_url)
+    if r.status_code != 200:
         return
-
-    launches = response.json()
+    launches = r.json()
 
     # Sort by date_unix
     launches.sort(key=lambda x: x.get("date_unix", 0))
 
     first_launch = launches[0]
 
-    # Get rocket info
-    rocket_id = first_launch.get("rocket")
-    rocket_name = (
-        requests.get(rockets_url + rocket_id).json().get("name", "unknown")
-    )
-
-    # Get launchpad info
-    launchpad_id = first_launch.get("launchpad")
-    lp_data = requests.get(launchpads_url + launchpad_id).json()
-    lp_name = lp_data.get("name", "unknown")
-    lp_locality = lp_data.get("locality", "unknown")
-
-    # Get launch name and date
+    # Get launch details
     launch_name = first_launch.get("name", "unknown")
     launch_date = first_launch.get("date_local", "unknown")
 
-    # Print formatted string
+    # Get rocket name
+    rocket_id = first_launch.get("rocket")
+    rocket_data = requests.get(rockets_url + rocket_id).json()
+    rocket_name = rocket_data.get("name", "unknown")
+
+    # Get launchpad info
+    launchpad_id = first_launch.get("launchpad")
+    launchpad_data = requests.get(launchpads_url + launchpad_id).json()
+    lp_name = launchpad_data.get("name", "unknown")
+    lp_locality = launchpad_data.get("locality", "unknown")
+
     print(
         f"{launch_name} ({launch_date}) {rocket_name} - {lp_name} ({lp_locality})"
     )
