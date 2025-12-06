5a14fc8187a62ef37dbf04dcfd176482284db88c
jordiindahood <hazem.ouni123@gmail.com>
2025-12-06 19:18:43 +0100

    script 8

---
+++ pipeline/pandas/8-prune.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" script 8 """
+
+
+def prune(df):
+    """
+    Removes entries where Close has NaN values.
+    Returns the modified DataFrame.
+    """
+    return df.dropna(subset=['Close'])
+    
