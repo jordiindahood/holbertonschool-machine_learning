a8a0602615158c549ed4dca569faf921e33c6bc8
jordiindahood <hazem.ouni123@gmail.com>
2025-12-17 17:03:24 +0100

    script 2

---
+++ pipeline/data_augmentation/1-crop.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" script 1 """
+import tensorflow as tf
+
+def crop_image(image, size):
+    """
+    Performs a random crop of an image.
+    """
+    return tf.image.random_crop(image, size)
